14d3eb2030b9acfc386f3f7c8ca9847555192cf7
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-01-31 12:52:43 -0300

    listagem de usuários + estilização

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Keyboard } from 'react-native'; //importa a api de teclado do react-native
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Container, Form, Input, 
+         SubmitButton, List, User, 
+         Avatar, Name, Bio, 
+         ProfileButton, ProfileButtonText 
+        } from './styles';
 
 import api from '../../services/api'; //acessando api do github
 
@@ -36,22 +40,40 @@ export default class Main extends Component {
 
     render () {
         const { users, newUser } = this.state;
+        
         return (
+
             <Container>
                 <Form>
                     <Input 
-                     autoCorrect={false}
-                     autoCapitalize="none"
-                     placeholder="Adicionar usuário"
-                     value={newUser}
-                     onChangeText={text => this.setState({ newUser: text })}
-                     returnKeyType="send"
-                     onSubmitEditing={this.handleAddUser}
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    placeholder="Adicionar usuário"
+                    value={newUser}
+                    onChangeText={text => this.setState({ newUser: text })}
+                    returnKeyType="send"
+                    onSubmitEditing={this.handleAddUser}
                     />
                     <SubmitButton onPress={this.handleAddUser}>
                         <Icon name="add" size={20} color="#fff" />
                     </SubmitButton>
                 </Form>
+
+                {/*listagem dos usuários add através da api*/}
+                <List 
+                    data={users} 
+                    keyExtractor={user => user.login}
+                    renderItem={( {item} ) => (  
+                        <User> 
+                            <Avatar source={{ uri: item.avatar }} /> 
+                            <Name>{item.name}</Name>
+                            <Bio>{item.bio}</Bio>
+                            <ProfileButton onPress={() => {}}>
+                                <ProfileButtonText>Ver perfil</ProfileButtonText>
+                            </ProfileButton>
+                        </User>
+                    )}
+                />
             </Container>
         );
     }
