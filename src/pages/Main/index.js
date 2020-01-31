b60c30c54660ca08e5052a2232e471879943eacc
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-01-31 11:24:45 -0300

    conexão api github + configuração sumir teclado no input

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Keyboard } from 'react-native'; //importa a api de teclado do react-native
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Form, Input, SubmitButton } from './styles';
@@ -11,8 +12,26 @@ export default class Main extends Component {
         users: []
     };
 
-    handleAddUser = () => {
-        console.tron.log(this.state.newUser);
+    handleAddUser = async () => { //await é assincrono
+        // console.tron.log(this.state.newUser);
+        const { users, newUser } = this.state; //para add um novo item na lista precisa da referência dos itens anteriores
+
+        /* conexão com a api */
+        const response = await api.get(`/users/${newUser}`); //rota
+
+        const data = { //selecionando apenas infomações que eu quero/preciso
+            name: response.data.name,
+            login: response.data.login,
+            bio: response.data.bio,
+            avatar: response.data.avatar_url
+        };
+
+        this.setState({
+            users: [ ...users, data ], //copia todos os usuários que estão dentro do estado (na array de users) e colocar os dados (data)
+            newUser: '' //pega o valor do input e reseta
+        });
+
+        Keyboard.dismiss();
     }
 
     render () {
